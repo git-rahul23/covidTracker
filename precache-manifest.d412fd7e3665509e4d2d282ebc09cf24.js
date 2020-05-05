62eb6fd8ad6b9bc8bea4ab19c931338429b90955
git-rahul23 <jr.rahul07@gmail.com>
2020-05-05 19:50:31 +0530

    Updates

---
+++ precache-manifest.d412fd7e3665509e4d2d282ebc09cf24.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f283b3ae3977e013db2b6dac19af6b4",
+    "url": "/covidTracker/index.html"
+  },
+  {
+    "revision": "46c29ddffc020121d765",
+    "url": "/covidTracker/static/css/main.8780b46e.chunk.css"
+  },
+  {
+    "revision": "105d78add5dcec527aa4",
+    "url": "/covidTracker/static/js/2.bf1503f4.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covidTracker/static/js/2.bf1503f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46c29ddffc020121d765",
+    "url": "/covidTracker/static/js/main.73efc8eb.chunk.js"
+  },
+  {
+    "revision": "7af15031f76f68134f6d",
+    "url": "/covidTracker/static/js/runtime-main.e0a5b9b7.js"
+  },
+  {
+    "revision": "73ae3a10a8c47e124f7ac5cdec840229",
+    "url": "/covidTracker/static/media/applestore.73ae3a10.PNG"
+  },
+  {
+    "revision": "da30159dadc13de852465c5e5205b03a",
+    "url": "/covidTracker/static/media/cover.da30159d.png"
+  },
+  {
+    "revision": "ddcfe16754aaf6e468dd37f7dfe8b029",
+    "url": "/covidTracker/static/media/covidLogo.ddcfe167.png"
+  },
+  {
+    "revision": "de0e664d5264b318982491ec29b22ed6",
+    "url": "/covidTracker/static/media/googleplay.de0e664d.PNG"
+  },
+  {
+    "revision": "86af3e90af743418872d5c0648a480fb",
+    "url": "/covidTracker/static/media/keepDist.86af3e90.png"
+  },
+  {
+    "revision": "6cecc8a7a18f245a8a8ce210e99cdb14",
+    "url": "/covidTracker/static/media/scan.6cecc8a7.PNG"
+  },
+  {
+    "revision": "5bffb54cb76c3fb05e77a81dc362399d",
+    "url": "/covidTracker/static/media/stayHome.5bffb54c.png"
+  },
+  {
+    "revision": "c9b88e14ad94549b9a46c58d425c11cc",
+    "url": "/covidTracker/static/media/washHands.c9b88e14.png"
+  }
+]);
